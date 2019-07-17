5d35609f93dda0ecd554b5e9a0061e45cc64757f
shencangsong <shencangsong@hc360.com>
2019-07-17 13:37:59 +0800

    '增加手机号验证'

--- src/Utils.php
+++ src/Utils.php
@@ -2,7 +2,25 @@
 namespace Mingxuan\Utils;
 
 class Utils{
-    public function curl_file_get_contents(){
-        return "ssssss ";
+
+    /**
+     * 正则验证手机号
+     * @param string $mobile
+     * @return bool
+     */
+    static function preg_mobile($mobile = ''){
+        if(preg_match("/^1[3456789]\d{9}$/", $mobile)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * utf8 字符长度
+     * @param string $str
+     * @return float|int
+     */
+    static function strlen_utf8($str = ''){
+        return (mb_strlen($str) + strlen($str))/2;
     }
 }
